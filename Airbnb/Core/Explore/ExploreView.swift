bf7779ab5c85ead2a5b998bb70d633b8a46be3d6
Riad <riad6723bd@gmail.com>
2024-09-16 21:16:16 +0600

    folders have been created

---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by Ajijul Hakim Riad on 16/9/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
