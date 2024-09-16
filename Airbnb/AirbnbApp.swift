b2c6d13b40c3aa722a9bf761df37f4eaac53ad9a
Riad <riad6723bd@gmail.com>
2024-09-16 20:48:58 +0600

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Ajijul Hakim Riad on 16/9/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
